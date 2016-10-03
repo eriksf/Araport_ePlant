89cb8b3db5e42a58bf09a85a58f2aba8ec8c3cc0
Asher <asher.pasha@utoronto.ca>
2016-10-03 18:13:52 -0400

    app/scripts directory is ready.

--- app/scripts/start.js
+++ app/scripts/start.js
@@ -1,3 +1,26 @@
+var cursorX;
+var cursorY;
+
+$(document).mousemove(function(e){
+	cursorX = e.clientX;
+	cursorY = e.clientY-15;
+});
+
+var test = function(event){
+	if(this.config.title){
+		this.DOM.header.css({height:"30px"});
+		this.DOM.title.css({height:"32px"});
+	}
+	else{
+
+		this.DOM.header.css({height:"0px"});
+		this.DOM.title.css({height:"0px"});
+	}
+	this.DOM.content.css({'max-height':document.body.clientHeight*0.8-20});
+	//this.DOM.content.css({'max-width':window.width*0.8});
+	this._reset();
+};
+
 var test = function(event){
 	if(this.config.title){
 		this.DOM.header.css({height:"30px"});
