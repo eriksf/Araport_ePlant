c1c588e1d04d13a931dbcca31de5467be940f60c
eriksf <eferlanti@tacc.utexas.edu>
2017-01-05 13:50:35 -0600

    updated start.js

--- app/scripts/start.js
+++ app/scripts/start.js
@@ -55,7 +55,7 @@ function inputBlur(i) {
 			// Deleting Araport css
 			$('link[rel=stylesheet][href*="https://www.araport.org/sites/default/files/css/"]').remove();
 			$('link[rel=stylesheet][href*="https://araport-dev.tacc.utexas.edu/sites/default/files/css/"]').remove();
-			$('link[rel=stylesheet][href*="https://jcvi03.tacc.utexas.edu/sites/default/files/css/"]').remove();
+			$('link[rel=stylesheet][href*="http://jcvi03.tacc.utexas.edu/sites/default/files/css/"]').remove();
 			$('link[rel=stylesheet][href*="https://maxcdn.bootstrapcdn.com/"]').remove();
 			/* Google maps initailiz */
 
